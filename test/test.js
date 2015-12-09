4381a41fc7aacd0d57a4eba2e65390a6ef73bbed
michaeldegroot <specamps@gmail.com>
2015-12-09 08:36:27 +0100

    not really happy with this test, there must be a better way

--- test/test.js
+++ test/test.js
@@ -2,12 +2,12 @@ var assert = require('assert');
 var assert = require('assert-plus');
 var clientIo = require('socket.io-client');
 var io = require('socket.io').listen(3000,{ log: false });
-var clientIo = require('socket.io-client');
 var antiSpam = require('../antispam');
 
 var passed = false;
 var passedBan = false;
 var disconnected = false;
+clientSocket = clientIo.connect('http://127.0.0.1:3000');
 
 var antiSpam = new antiSpam({
 	spamCheckInterval: 3000,
@@ -20,6 +20,25 @@ var antiSpam = new antiSpam({
 	debug: true
 });
 
+function repeat(){
+	if(disconnected) return;
+	clientSocket.emit('spamming', { some: 'data' });
+	setTimeout(function(){ repeat(); },250);
+}
+clientSocket.on("connect", function(){
+	repeat();
+});
+clientSocket.on("disconnect", function(){
+	disconnected = true;
+});
+clientSocket.on("spamscore", function(score){
+});
+setInterval(function(){
+	if(!disconnected) return;
+	if(passed == true) passedBan = true;
+	passed = true;
+},10);
+
 io.sockets.on('connection', function (socket) {
 	antiSpam.onConnect(socket);
 	socket.on("spamming", function() {
@@ -27,40 +46,10 @@ io.sockets.on('connection', function (socket) {
 	});
 });
 
-
-describe("Socket.io", function(){
-	it('Connect', function(done){
-		socket = clientIo.connect('http://127.0.0.1:3000');
-		io.sockets.on('connection', function (socket) {
-			clientSocket = socket;
-			done();
-		});
-	});
-});
-/**
 describe("Internal", function(){
-	this.timeout(60000);
+	this.timeout(5000);
 	var passedInt;
 	it('Connect to the webserver, spam socket.emits and get disconnect/kicked', function(done){
-		var socket = clientIo.connect('http://127.0.0.1:3000');
-		socket.on("connect", function(){
-			repeat();
-		});
-		socket.on("disconnect", function(){
-			disconnected = true;
-		});
-		socket.on("spamscore", function(score){
-		});
-		setInterval(function(){
-			if(!disconnected) return;
-			if(passed == true) passedBan = true;
-			passed = true;
-		},10);
-		function repeat(){
-			if(disconnected) return;
-			socket.emit('spamming', { some: 'data' });
-			setTimeout(function(){ repeat(); },250);
-		}
 		passedInt = setInterval(function(){
 			if(passed == true){
 				clearInterval(passedInt);
@@ -69,5 +58,4 @@ describe("Internal", function(){
 		},10);
 	});
 });
-*/
 
